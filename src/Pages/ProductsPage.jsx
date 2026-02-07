c037910a76eddbef8fc613839bbf3f055a78e28d
Faezeh <faezehadabi9@gmail.com>
2026-02-07 21:16:09 +0330

    add loader-spinner

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,12 +1,13 @@
 import Card from "../Components/Card";
+import Loader from "../Components/Loader";
 import { useProducts } from "../Context/ProductContext";
-import styles from "./ProductPage.module.css"
+import styles from "./ProductPage.module.css";
 function ProductsPage() {
   const Products = useProducts();
   return (
     <div className={styles.container}>
       <div className={styles.products}>
-        {!Products.length && <p>Loading</p> }
+        {!Products.length &&  <Loader />}
         {Products.map((p) => (
           <Card key={p.id} data={p} />
         ))}
