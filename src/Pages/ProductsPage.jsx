967de86da4bee60cab38987cf4b0579085b13411
Faezeh <faezehadabi9@gmail.com>
2026-02-07 22:21:32 +0330

    create categories section

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -4,10 +4,16 @@ import Loader from "../Components/Loader";
 import { useProducts } from "../Context/ProductContext";
 import styles from "./ProductPage.module.css";
 import { useState } from "react";
+import { FaListUl } from "react-icons/fa";
 function ProductsPage() {
   const Products = useProducts();
   const [search, setSearch] = useState("");
-  const searchHandler = () => {
+  const searchHandler = () => {};
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    console.log(category);
   };
   return (
     <>
@@ -29,7 +35,19 @@ function ProductsPage() {
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>SideBar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronices</li>
+            <li>Jewelery</li>
+            <li>men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
